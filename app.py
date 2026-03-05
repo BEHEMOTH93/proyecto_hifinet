ecce674000c618d67b4ac3837513e955b15a319f
mn-moyano <mn.moyanoz@uea.edu.ec>
2026-03-04 22:23:56 -0500

    Sistema Avanzado de Gestión de Inventario - Semana 12

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, url_for, request, redirect, flash
 from form import ProductoForm
 from inventario.bd import init_db
 from inventario.inventario import Inventario
+import json
+import csv
 
 app = Flask(__name__)
 # Clave obligatoria para que funcionen los formularios Flask-WTF
@@ -16,12 +18,78 @@ def index():
     form = ProductoForm()
     # Si el formulario es válido al presionar el botón guardar:
     if form.validate_on_submit():
-        inventario.añadir(form.nombre.data, form.descripcion.data, form.cantidad.data, form.precio.data)
+        nombre = form.nombre.data
+        descripcion = form.descripcion.data
+        cantidad = form.cantidad.data
+        precio = form.precio.data
+        inventario.añadir(nombre, descripcion, cantidad, precio)
+        ruta_txt = "inventario/data/datos.txt"
+        ruta_json = "inventario/data/datos.json"
+        ruta_csv = "inventario/data/datos.csv"
+
+        #TXT
+        with open(ruta_txt, "a") as f:
+            f.write(f"{nombre},{descripcion},{cantidad},{precio}\n")
+        
+        #JSON
+        nuevo = {
+            "nombre": nombre,
+            "descripcion": descripcion,
+            "cantidad": cantidad,
+            "precio": precio
+        }
+        try:
+            with open(ruta_json, "r") as f:
+                datos = json.load(f)
+        except:
+            datos = []
+        
+        datos.append(nuevo)
+
+        with open(ruta_json, "w") as f:
+            json.dump(datos, f, indent=4)
+        
+        #CSV
+        with open(ruta_csv, "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([nombre, descripcion, cantidad, precio])
         flash('Equipo añadido exitosamente al inventario.')
         return redirect(url_for('index'))
         
     inventario.cargar_desde_db()
     return render_template('index.html', productos=inventario.productos_dict.values(), form=form)
+
+@app.route('/datos')
+def datos():
+    datos_txt = []
+    datos_csv = []
+    datos_json = []
+
+    #TXT
+    try:
+        with open("inventario/data/datos.txt") as f:
+            for linea in f:
+                datos_txt.append(linea.strip().split(","))
+    except:
+        pass
+
+    #JSON
+    try:
+        with open("inventario/data/datos.json") as f:
+            datos_json = json.load(f)
+    except:
+        pass
+
+    #CSV
+    try:
+        with open("inventario/data/datos.csv") as f:
+            reader = csv.reader(f)
+            for fila in reader:
+                datos_csv.append(fila)
+    except:
+        pass
+
+    return render_template("datos.html", datos_txt=datos_txt, datos_json=datos_json, datos_csv=datos_csv)
 
 @app.route('/eliminar/<int:id>')
 def eliminar(id):
